10ff613631ccdedc3bcf4f996d5790963fd552a0
oluthje <oluthje@gmail.com>
2017-10-20 09:30:49 -0700

    Added eightball functionality
    eightball webpage can now tell your fortune.

--- jscrip.js
+++
@@ -1,7 +0,0 @@
-function testJS() {
-	alert("test!");
-}
-
-function shake8ball() {
-	document.getElementById("magic8ball").src="magic8ball2.jpg";
-}
--- jscript.js
+++ jscript.js
@@ -2,7 +2,26 @@ function testJS() {
 	alert("test!");
 }
 
+// this is a comment, I'm useless.
 function shake8ball() {
-	document.getElementById("magic8ball").src="magic8ball2.jpg";
-	alert("We shall see!")
+	//change image on click.
+//	document.getElementById("magic8ball").src="magic8ball2.jpg";
+	
+	//changes 8 ball text.
+	document.getElementById("results").innerHTML=list[randNumber()];
+}
+
+var list = ["It is so",
+			"We shall see",
+			"Yes",
+			"Ask again",
+			"Signs point to yes",
+			"No",
+			"Try again later",
+			"Doubtful",
+			"Certainly",
+			"Its likely"];
+
+function randNumber () {
+	return Math.floor(Math.random() * 10);
 }
